4094257fe591ca08a7b64eafd58741b52429ad29
haider <haidertiger10@gmail.com>
2020-04-03 04:25:02 +0500

    store user data to firestore and app state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -14,17 +14,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      current: null
+      currentUser: null
     };
   }
 
+  // for using unmounting method
   unsubscribeFromAuth = null;
 
+  // store data on firestore and state
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        // store the data in firestore and get data back
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        // store data from firestore to state
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,9 @@
+// import firebase libraries
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+// firebase config object from firebase console
 const config = {
   apiKey: "AIzaSyAl-E7TezE_hUH9QLXVegrG9w3X72tlBSs",
   authDomain: "crwn-db-65f80.firebaseapp.com",
@@ -13,11 +15,40 @@ const config = {
   measurementId: "G-TN40FHGKV0"
 };
 
+// initialize firebase app
 firebase.initializeApp(config);
 
+// store user data in firebase database --- firestore
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error);
+    }
+  }
+
+  return userRef;
+};
+
+// export some firebase libraries
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// add provider
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
