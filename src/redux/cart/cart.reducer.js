9fad09c619f9fc510ac906bae74b5e6dbd0f0bbe
haider <haidertiger10@gmail.com>
2020-05-09 22:01:52 +0500

    add funcationality of increase and decrease items in checkout page

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,6 @@
 import { CartActionTypes } from "./cart.types";
 
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -20,6 +20,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      }
 
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
